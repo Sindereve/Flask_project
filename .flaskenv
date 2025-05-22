399958ca7c3af8a2fcfcd9a1c8a591c9cbf00368
Argo <sindereve850live@gmail.com>
2025-05-22 22:45:01 +0500

    Create logger

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,2 @@
 FLASK=APP=main.py
+FLASK_DEBUG=0
--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import os
+import logging
+from logging.handlers import RotatingFileHandler  # ,SMTPHandler (добавить, если будем использовать уведомления по email)
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,4 +14,42 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/my_flask.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('My_flask startup')
+# ========================================================================================================
+#     Тут мы можем настроить отправку инфу об ошибке на почту с помощью яндекс почты
+#     больше инфы тут https://yandex.ru/support/yandex-360/customers/mail/ru/mail-clients/others.html
+#     Мы в россии, а что в рунете всегда стабильно
+#     работа яндекс почты... Да..Да....
+#     if app.config['MAIL_SERVER']:
+#         auth = None
+#         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+#             auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+#         secure = None
+#         if app.config['MAIL_USE_TLS']:
+#             secure = ()
+#         mail_handler = SMTPHandler(
+#             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+#             fromaddr=app.config['MAIL_USERNAME'],
+#             toaddrs=app.config['ADMINS'], subject='Flask Proj Failure',
+#             credentials=auth, secure=secure
+#         )
+#         mail_handler.setLevel(logging.ERROR)
+#         app.logger.addHandler(mail_handler)
+# ========================================================================================================
+
+
+
+from app import routes, models, errors
