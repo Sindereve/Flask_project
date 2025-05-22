a9c5719fd79e1e95e759a7be889f0a2d7855821c
Acer_sindik <sindereve850live@gmail.com>
2025-05-22 17:40:25 +0500

    create registrate Form

--- app/models.py
+++ app/models.py
@@ -2,19 +2,38 @@ from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from hashlib import md5
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     tablename = 'users'
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
-    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(60), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
     
+    def set_password(self, passwod):
+        self.password_hash = generate_password_hash(passwod)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=retro&s={size}'
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
-    
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
 class Post(db.Model):
     id:so.Mapped[int] = so.mapped_column(primary_key=True)
     body: so.Mapped[str] = so.mapped_column(sa.String(140))
